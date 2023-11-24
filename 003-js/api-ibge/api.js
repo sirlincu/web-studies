fe80a5d307fa0b013c5c28a076c5eff963a992bc
Lincoln Rebouças <77850371+sirlincu@users.noreply.github.com>
2023-11-24 19:16:20 -0300

    code aula 9

--- 003-js/api-ibge/api.js
+++ 003-js/api-ibge/api.js
@@ -6,10 +6,41 @@ function carregarEstados() {
 
     const estados = await response.json();
     console.log(estados);
+
+    preencherSelectEstados(estados);
+
 }
 
-function carregarCidades() {
+async function carregarCidades() {
+    limparSelect("cidades");
 
+    const selectEstados = document.getElementById("estados");
+
+    const estado_value = selectEstados.options[estado_index].value;
+
+    if (estado_index === 0 || estado_value === "") {
+        return;
+    }
+
+    const response = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estado_value}/municipios`)
+
+    const cidades = await response.json();
+    preencherSelectCidades(cidades);
+}
+
+function preencherSelectCidades(cidades) {
+    const selectCidades = document.getElementById("cidades");
+
+    for(const cidade of cidades) {
+        const { id, nome } = cidade;
+
+        const option = document.createElement("option");
+
+        option.value = id;
+        option.innerHTML = nome;
+
+        selectCidades.appendChild(option);
+    }
 }
 
 function preencherSelectEstados(estados) {
@@ -17,14 +48,21 @@ function preencherSelectEstados(estados) {
         console.log(estados[i].nome);
     }
 
-    limparSelectEstados();
+    limparSelectEstados()
+
+    const select = document.getElementById("estados");
+
+    estados.sort( (a, b) => a.nome.localeCompare(b.nome) );
+
+    const regiao_norte = estados.filter( (estado) => estado.regiao.id === 3);
+    console.log(regiao_norte);
 
     for(let estado of estados) {
         // const nome = estado.nome;
         // const uf = estado.sigla;
 
         // DESTRUCTING 
-        const { nome, sigla } = estado;
+        const { id, nome, sigla } = estado;
         console.log(nome, sigla);
 
         const option = document.createElement("option");
@@ -51,3 +89,24 @@ function limparSelectEstados() {
         select.remove(1);
     }
 }
+
+function limparSelect(campo) {
+    const select = document.getElementById(campo);
+
+    while(select.length > 1) {
+        select.remove(1);
+    }
+}
+
+// Classica
+function soma (a, b) {
+    return a + b;
+}
+
+// Arrow functions
+const somaArrow = (a, b) => {
+    return a + b;
+}
+
+// Arrow function in line: retorno direto
+const somaArrowInLine = (a, b) => a + b;
